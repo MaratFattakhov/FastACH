5ebfe8ecd471c2674232fefe0b931754a3f1c99e
Marat Fattakhov <marat.fattakhov@asuresoftware.com>
2024-01-25 15:10:15 -0600

    Added Benchmarks

---
+++ tests/FastACH.Benchmarks/FakeAchFileGenerator.cs
@@ -0,0 +1,81 @@
+﻿using FastACH.Records;
+
+namespace FastACH.Benchmarks
+{
+    internal class FakeAchFileGenerator
+    {
+        public static async Task CreateAchFile(string path, int numberOfEntries)
+        {
+            var achFile = CreateAchFile(numberOfEntries / 10, 10);
+            var writer = new AchFileWriter();
+            await writer.WriteToFile(achFile, path);
+        }
+
+        private static AchFile CreateAchFile(int numberOfBatches, int numberOfEntriesInEachBatch)
+        {
+            var achFile = new AchFile()
+            {
+                OneRecord = new FileHeaderRecord()
+                {
+                    ImmediateDestination = "123456789",
+                    ImmediateOrigin = "123456789",
+                    FileCreationDate = DateOnly.FromDateTime(DateTime.Now),
+                    FileCreationTime = TimeOnly.FromDateTime(DateTime.Now),
+                    FileIdModifier = 'A',
+                    ImmediateDestinationName = "PNC Bank",
+                    ImmediateOriginName = "Microsoft Inc.",
+                    ReferenceCode = "00000000"
+                },
+                BatchRecordList =
+                    Enumerable.Range(0, numberOfBatches)
+                        .Select(_ => CreateBatchRecord(numberOfEntriesInEachBatch)).ToList()
+            };
+
+            return achFile;
+        }
+
+
+        private static BatchRecord CreateBatchRecord(int numberOfEntries)
+        {
+            return new BatchRecord()
+            {
+                BatchHeader = new BatchHeaderRecord()
+                {
+                    ServiceClassCode = 200,
+                    CompanyName = "companyName",
+                    CompanyDiscretionaryData = "companyDiscretionary",
+                    CompanyId = "companyID",
+                    CompanyEntryDescription = "EntryDescr",
+                    CompanyDescriptiveDate = new DateOnly(2011, 02, 03),
+                    EffectiveEntryDate = new DateOnly(2011, 01, 02),
+                    OriginatingDFIID = "DFINumber"
+                },
+                TransactionDetailsList = Enumerable.Range(0, numberOfEntries).Select(_ => CreateTransactionDetails()).ToList(),
+            };
+        }
+
+        private static TransactionDetails CreateTransactionDetails()
+        {
+            return new TransactionDetails
+            {
+                EntryDetail = new EntryDetailRecord()
+                {
+                    TransactionCode = 22,
+                    ReceivingDFIID = 12345678,
+                    CheckDigit = '9',
+                    DFIAccountNumber = "1313131313",
+                    Amount = 22M,
+                    ReceiverIdentificationNumber = "ID Number",
+                    ReceiverName = "ID Name",
+                    DiscretionaryData = "Desc Data",
+                    AddendaRecordIndicator = false,
+                },
+                Addenda = null
+                //Addenda = new AddendaRecord()
+                //{
+                //    AddendaInformation = "Monthly bill"
+                //}
+            };
+        }
+    }
+}
---
+++ tests/FastACH.Benchmarks/Program.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FastACH;
+using FastACH.Benchmarks;
+
+var summary = BenchmarkRunner.Run<FastACHvsChoetlNacha>();
+
+public class FastACHvsChoetlNacha
+{
+    const string achFileName = "test.ach";
+
+    [Params(1000, 10000, 100000)]
+    public int NumberOfEntries { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        FakeAchFileGenerator.CreateAchFile(achFileName, NumberOfEntries).Wait();
+    }
+
+    [Benchmark]
+    public async Task<AchFile> FastACH()
+    {
+        var reader = new AchFileReader();
+        var achFile = await reader.Read(achFileName);
+        if (achFile.BatchRecordList.Count != (NumberOfEntries / 10))
+        {
+            throw new Exception("Incorrect number of batches : " + achFile.BatchRecordList.Count);
+        }
+        return achFile;
+    }
+}
