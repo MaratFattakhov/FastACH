90de8106c600cbaed41c9293e2219f90f3bc0079
Marat Fattakhov <marat.fattakhov@asuresoftware.com>
2024-06-03 22:49:15 -0500

    0.2.3 added more transaction codes

--- src/FastACH/TransactionCodes.cs
+++ src/FastACH/TransactionCodes.cs
@@ -2,8 +2,8 @@
 {
     internal static class TransactionCodes
     {
-        public static List<uint> CreditCodes = new List<uint>() { 22, 32, 42 };
-        public static List<uint> DebitCodes = new List<uint>() { 27, 37, 47 };
+        public static List<uint> CreditCodes = new List<uint>() { 22, 32, 42, 52, 23, 33, 43, 53, 48 };
+        public static List<uint> DebitCodes = new List<uint>() { 27, 37, 47, 55, 28, 38 };
 
         public static bool IsCredit(uint code)
         {
