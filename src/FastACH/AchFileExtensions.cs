5997f26c31d754a17c0fdc5c89f426767759071a
Marat Fattakhov <marat.fattakhov@asuresoftware.com>
2024-06-27 18:32:21 -0500

    0.3.2 returned GetRoutingNumber

--- src/FastACH/AchFileExtensions.cs
+++ src/FastACH/AchFileExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace FastACH
+﻿using FastACH.Records;
+
+namespace FastACH
 {
     public static class AchFileExtensions
     {
@@ -13,6 +15,12 @@
             }
 
             return true;
+        }
+
+        public static string GetRoutingNumber(this EntryDetailRecord record)
+        {
+            var routingNumberString = record.ReceivingDFIID.ToString().PadLeft(8, '0');
+            return routingNumberString + record.CheckDigit;
         }
 
         private static decimal GetTotalCreditEntryDollarAmount(BatchRecord batch) => batch.TransactionRecords.Where(x => TransactionCodes.IsCredit(x.EntryDetail.TransactionCode)).Sum(x => Math.Round(x.EntryDetail.Amount, 2, MidpointRounding.AwayFromZero));
